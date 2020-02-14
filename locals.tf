1bb3b0ad16cbffe7df49082e3d06701503a2b7fa
Kerim Satirli <k.satirli@gmail.com>
2020-02-14 13:33:50 +0100

    linting

--- locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  zone_name           = data.aws_route53_zone.zone.name // NOTE: trailing period is added by data source
-  verification_record = "keybase-site-verification=${var.verification_record}"
-}
