e98ec770573c6790e38f3d6a71af9cec0919c537
Kerim Satirli <k.satirli@gmail.com>
2020-02-14 13:34:33 +0100

    adds locals to variables

--- variables.tf
+++ variables.tf
@@ -17,3 +17,8 @@ variable "verification_record" {
 data "aws_route53_zone" "zone" {
   zone_id = var.zone_id
 }
+
+locals {
+  zone_name           = data.aws_route53_zone.zone.name // NOTE: trailing period is added by data source
+  verification_record = "keybase-site-verification=${var.verification_record}"
+}
