ab1d85acc1be174bca25459527a68b7990a05f8b
Kerim Satirli <k.satirli@gmail.com>
2022-09-02 15:02:07 +0200

    updates descriptions

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "zone_id" {
   type        = string
-  description = "ID of the DNS Zone to store Records in"
+  description = "ID of the DNS Zone to store DNS Record in."
 }
 
 variable "record_ttl" {
   type        = string
-  description = "TTL for all DNS records"
+  description = "TTL for the DNS record."
   default     = 300
 }
 
 variable "domain_proof" {
   type        = string
-  description = "Domain Proof TXT Record (without `keybase-site-verification=` prefix)"
+  description = "Domain Proof TXT Record (without `keybase-site-verification=` prefix)."
 }
 
 data "aws_route53_zone" "zone" {
@@ -19,6 +19,6 @@ data "aws_route53_zone" "zone" {
 }
 
 locals {
-  zone_name    = data.aws_route53_zone.zone.name // NOTE: trailing period is added by data source
+  zone_name    = data.aws_route53_zone.zone.name # NOTE: trailing period is added by data source
   domain_proof = "keybase-site-verification=${var.domain_proof}"
 }
